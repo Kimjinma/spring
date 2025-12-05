68054f5384805dbcd112ed491c0f282ac58b3b3e
Jinma <akflzk33@naver.com>
2025-12-05 22:29:07 +0900

    Exception적용,  commit

--- src/main/java/com/example/demo/exception/AroundHubExceptionHandler.java
+++ src/main/java/com/example/demo/exception/AroundHubExceptionHandler.java
@@ -1,36 +1,24 @@
-package com.example.demo.controller;
+package com.example.demo.exception;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.hibernate.sql.ast.SqlTreeCreationLogger.LOGGER;
+@RestControllerAdvice
 
-@RestController
-public class HelloController {
-
-    //@RequestMapping (value = "/hello")
-    @GetMapping("hello")
-    public String hello(){
-        return "hello world";
-    }
-
-
+public class AroundHubExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
         HttpHeaders responseHeaders = new HttpHeaders();
         // responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-
-        LOGGER.info(e.getMessage());
-        LOGGER.info("Controller 내 ExceptionHandler 호출");
+        LOGGER.error("Advice 내 ExceptionHandler 호출, {}, {}");
 
         Map<String, String> map = new HashMap<>();
         map.put("error type", httpStatus.getReasonPhrase());
