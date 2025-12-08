37c6a0e9e46399e2cc54e9141fe664ec57987eb0
Jinma <akflzk33@naver.com>
2025-12-08 19:49:49 +0900

    RestTemplate 적용,  commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +19,10 @@ public class HelloController {
         return "hello world";
     }
 
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception {
+        throw new Exception();
+    }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
---
+++ src/main/java/com/example/demo/controller/RestTemplateController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import com.example.demo.data.service.RestTemplateService;
+import com.example.demo.dto.MemberDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rest-template")
+public class RestTemplateController implements RestTemplateService {
+
+    RestTemplateService restTemplateService;
+
+    @Autowired
+    public RestTemplateController(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @GetMapping(value = "/around-hub")
+    public String getAroundHub() {
+        return restTemplateService.getAroundHub();
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        return restTemplateService.getName();
+    }
+
+    @GetMapping(value = "/name2")
+    public String getName2() {
+        return restTemplateService.getName2();
+    }
+
+    @PostMapping(value = "/dto")
+    public ResponseEntity<MemberDTO> postDto() {
+        return restTemplateService.postDto();
+    }
+
+    @PostMapping(value = "/add-header")
+    public ResponseEntity<MemberDTO> addHeader() {
+        return restTemplateService.addHeader();
+    }
+}
